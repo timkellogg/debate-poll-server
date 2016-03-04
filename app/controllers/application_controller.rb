f77e19a19c00e8965381a61678abe16617afd27d
timkellogg <tim.kellogg@gmail.com>
2016-03-03 17:11:02 -0800

    cors cors cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
     # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
     # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
     # the browser will not reject the response                                                                                                                                                                                                                                             
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+    response.headers['Access-Control-Allow-Origin'] = '*'                                                                                                                                                                                                     
     response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
   end 
 
