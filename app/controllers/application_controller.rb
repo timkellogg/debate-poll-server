803e59ccec4b54a810b56bc61017fa0eb6676506
timkellogg <tim.kellogg@gmail.com>
2016-03-03 16:42:24 -0800

    change up headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
   # For all responses in this controller, return the CORS access control headers.
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'http://debatevote.s3-website-us-west-2.amazonaws.com/'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
   end
