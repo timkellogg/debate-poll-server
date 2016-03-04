27406ccb4d143033ba937dfc9fb26569bb48ed06
timkellogg <tim.kellogg@gmail.com>
2016-03-03 16:49:16 -0800

    trash the controller method cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,4 @@
 class ApplicationController < ActionController::API
-  before_action :cors_set_access_control_headers
 
-  # For all responses in this controller, return the CORS access control headers.
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://debatevote.s3-website-us-west-2.amazonaws.com/'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
 end
+
