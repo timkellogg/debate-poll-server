631c8cb0bb6a50b9e1b08ddbcf224126cec9f5d2
timkellogg <tim.kellogg@gmail.com>
2016-03-03 16:17:15 -0800

    restructure cors rack config

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Server
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
       
       allow do
         origins '*'
